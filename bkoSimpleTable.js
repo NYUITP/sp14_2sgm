86de4555f367a85a4c4d5546fccd51791caa79e8
Di Wu <dw1157@nyu.edu>
2014-03-07 18:11:34 -0500

    PieChartingWithoutUserInputWorking

---
+++ bkoSimpleTable.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+    beaker.bkoDirective("SimpleTable", function () {
+        return {
+            template: '<div id="container"></div>',
+            link: function (scope, element, attrs) {
+                //scope.x=document.getElementById("container")  //Find the element
+                //scope.x.innerHTML="Hello JavaScript";    
+$('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2010'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+            }
+        };
+    });
+})();
