3f28e455b2c934b19c424d1b3bac164a01cc2651
Di Wu <dw1157@nyu.edu>
2014-03-12 19:34:49 -0400

    Separate Line and Pie High Chart works
    SimpleOutputs does not work

---
+++ bkoSimpleOutputs.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+    beaker.bkoDirective("SimpleLine", function () {
+ return {
+  template: '<div id="container"></div>',
+  link: function (scope, element, attrs) {
+  function getColumnValues(obj, col, row){
+                var arr = new Array();
+                for(var i = 0; i < row; i++) {
+                  arr[i] = new Array();
+                  for(var j = 0; j < col; j++) {
+                    var out=jsObj.values[i][j+1].trim();
+                    if(j==1) out=parseInt(out);
+                    arr[i][j] = out;
+                  }
+                }
+                return arr;
+              }
+        function getColumnList(obj, col, row){
+                var arr = new Array();
+                for(var i = 0; i < row; i++) {
+                  arr[i] = new Array();
+                  for(var j = 0; j < col; j++) {
+        var out = jsObj.values[i][j].trim();
+                    arr[i][j] = out;
+                  }
+                }
+                return arr;
+              }
+
+
+              var jsObj = scope.model.getCellModel();
+              var dataArr = getColumnValues(jsObj, jsObj.columnNames.length-1, jsObj.values.length);
+        var columnList = getColumnList(jsObj, jsObj.columnNames.length-1, jsObj.values.length);
+
+        $('#container').highcharts({
+            title: {
+                text: 'Line Chart',
+                x: -20 //center
+            },
+            xAxis: {
+                categories: columnList
+            },
+            yAxis: {
+                title: {
+                    text: 'Values'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 10,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+    name: jsObj.columnNames[2],
+    data: dataArr
+      }]
+    });
+         }
+     };
+    });
+})();
--- bkoSimpleTable.js
+++ bkoSimpleTable.js
@@ -21,146 +21,9 @@
     'use strict';
     beaker.bkoDirective('SimpleTable', ["bkCellPluginManager", function (bkCellPluginManager) {
         return {
-            template: '<div>'
-            + '<select id=selectedOutput>'
-            +   '<option value="table">Table</option>'
-            +   '<option value="pie chart">Pie Chart</option>'
-            + '</select></br>'
-            + '<input type="button" ng-click="getOutputDisplay()" value="Run">'
-            //+   '<form ng-sumbit="scope.myFunction()">'
-            //+   'Chart Title: <input id="title" type="text"><br>'
-            //+   'Unit:        <input id="unit" type="text"><br>'
-            //+   '<input type="button" value="Submit" onclick="link.myFunction()">'
-            //+   '<input type="submit" value="Submit">'
-            //+   '</form>'
-            + '<div id="container"></div>'
-            + '</div>',
-            controller: function ($scope) {
-                $scope.getShareMenuPlugin = function () {
-                    // the following cellType needs to match
-                    //plugin.cellType = "bkTableDisplay"; in dynamically loaded outputDisplay_bkTableDisplay.js
-                    var cellType = "bkTableDisplay";
-                    return bkCellPluginManager.getPlugin(cellType);
-                };
-                $scope.$watch("getShareMenuPlugin()", function (getShareMenu) {
-                    if (getShareMenu && $scope.model.resetShareMenuItems) {
-                        $scope.model.resetShareMenuItems(getShareMenu($scope));
-                    }
-                });
-
-                var onDestroy = function () {
-                    //$scope.dt.fnDestroy();
-                    delete $scope.grid;
-                };
-                $scope.$on("$destroy", onDestroy);
-                $scope.getColumns = function () {
-                    var columns = _.map($scope.model.getCellModel().columnNames, function (col) {
-                        return {id: col, name: col, field: col, sortable: true};
-                    });
-                    var elm = document.createElement('div');
-                    $(elm).addClass('ui-widget').addClass('slick-cell');
-                    var getWidth = function (text) {
-                        $(elm).html(text);
-                        $('body').append(elm);
-                        var width = $(elm).width();
-                        $(elm).remove();
-                        return width + 10;
-                    };
-
-                    _.each(columns, function (col) {
-                        col.width = getWidth(col.field);
-                    });
-                    var r, c, row, col, width;
-                    for (r = 0; r < $scope.model.getCellModel().values.length && r < 10; ++r) {
-                        row = $scope.model.getCellModel().values[r];
-                        for (c = 0; c < columns.length; c++) {
-                            width = getWidth(row[c]);
-                            col = columns[c];
-                            if (width > col.width) {
-                                col.width = width;
-                            }
-                        }
-                    }
-                    var timeCol = _.find(columns, function (it) {
-                        return it.field === "time";
-                    });
-                    if (timeCol) {
-                        // if the server provides the converted timeStrings, just use it
-                        if ($scope.model.getCellModel().timeStrings) {
-                            var timeStrings = $scope.model.getCellModel().timeStrings;
-                            timeCol.width = getWidth(timeStrings[0]);
-                            timeCol.formatter = function (row, cell, value, columnDef, dataContext) {
-                                return timeStrings[row];
-                            };
-                        } else {
-                            timeCol.width = getWidth("20110101 23:00:00.000 000 000");
-                            timeCol.formatter = function (row, cell, value, columnDef, dataContext) {
-                                var nano = value % 1000;
-                                var micro = (value / 1000) % 1000;
-                                var milli = value / 1000 / 1000;
-                                var d = new Date(milli);
-                                var doubleDigit = function (integer) {
-                                    if (integer < 10) {
-                                        return "0" + integer;
-                                    }
-                                    return integer.toString();
-                                };
-                                var trippleDigit = function (integer) {
-                                    if (integer < 10) {
-                                        return "00" + integer;
-                                    } else if (integer < 100) {
-                                        return "0" + integer;
-                                    }
-                                    return integer.toString();
-                                };
-                                var result = "";
-                                result += d.getFullYear() + doubleDigit(d.getMonth() + 1) + doubleDigit(d.getDate());
-                                result += " ";
-                                result += doubleDigit(d.getHours()) + ":" + doubleDigit(d.getMinutes()) + ":" + doubleDigit(d.getSeconds());
-                                result += ".";
-                                result += trippleDigit(d.getMilliseconds());
-                                result += " " + trippleDigit(micro);
-                                result += " " + trippleDigit(nano);
-                                return result;
-                            };
-                        }
-                    }
-                    return columns;
-                };
-                $scope.getOptions = function () {
-                    var options = {
-                        enableCellNavigation: true,
-                        enableColumnReorder: true,
-                        multiColumnSort: true,
-                        selectedCellCssClass: 'bk-table-cell-selected'
-                        //forceFitColumns: true
-                    };
-                    return options;
-                };
-                $scope.getData = function () {
-                    var data = _.map($scope.model.getCellModel().values, function (row) {
-                        return _.object($scope.model.getCellModel().columnNames, row);
-                    });
-                    return data;
-                };
-            },
+            template: '<div id="container"></div>',
             link: function (scope, element, attrs) {
- 
-              //var title = document.getElementById("title").value;  
-              //var unit = document.getElementById("unit").value;
-              scope.getOutputDisplay=function(){
-                var outputType = document.getElementById("selectedOutput");
-                if(outputType.selectedIndex==0){
-                  outputTable();
-                }
-                else if(outputType.selectedIndex==1) {
-                  outputPieChart();
-                }
-              };
-              function outputTable(){
 
-              };
-              function outputPieChart(){
               function convertToArray(obj, col, row){
                 var arr = new Array();
                 for(var i = 0; i < row; i++) {
@@ -217,7 +80,6 @@
 
                 }]
               });
-              };
             }
         };
     }]);
