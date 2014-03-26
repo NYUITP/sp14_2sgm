5c7a2fc312cb32e32b44b39c3e9e4afa841a3e9c
Di Wu <dw1157@nyu.edu>
2014-03-26 17:31:18 -0400

    Name changes

--- bkoFlotr2pie.js
+++ bkoFlotr2pie.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    beaker.bkoDirective("pie43", function () {
+    beaker.bkoDirective("flotr2_pie", function () {
  return {
 	template: '<div id="container" style="width:600px;height:384px;margin:8px auto"></div>',
 	link: function (scope, element, attrs) {
