eead2bddea659b98eacf9eb967557a95025e1652
Di Wu <dw1157@nyu.edu>
2014-04-08 10:42:08 -0400

    Angular JS: Change object

--- bkoFlotr2Line.js
+++ bkoFlotr2Line.js
@@ -5,17 +5,21 @@
       return {
             template: '<div>'
             + '<b>Title&nbsp;</b> <input type="text" ng-model="title"  size="30" placeholder="Add graph title here"></br>'
-            + '<b>X Axis&nbsp;</b><select ng-model="xaxis" ng-options="colOption for colOption in colOptions"><option value="">-- choose x-axis --</option></select>' 
-            + '<b>Y Axis&nbsp;</b><select ng-model="yaxis" ng-options="colOption for colOption in colOptions"><option value="">-- choose y-axis --</option></select>' 
+            + '<b>X Axis&nbsp;</b><select ng-model="xaxis" ng-options="colOption.colName for colOption in colOptions"><option value="">-- choose x-axis --</option></select>' 
+            + '<b>Y Axis&nbsp;</b><select ng-model="yaxis" ng-options="colOption.colName for colOption in colOptions"><option value="">-- choose y-axis --</option></select>' 
             + '<input type="button" value="Add Line" ng-click="addLine()"></br>'
             + '<b>Line Groups [x,y]:</b></br>'
             + '<ul>'
             +   '<li ng-repeat="line in lineGroup">'
             +      '<input type="button" value="Remove" ng-click="removeLine(line.x, line.y)">'
-            +     '&nbsp;&nbsp;[{{line.x}}&nbsp;,&nbsp;{{line.y}}]'
+            +     '&nbsp;&nbsp;[{{line.x.colName}}&nbsp;,&nbsp;{{line.y.colName}}]'
             +   '</li>'
             + '</ul>'
+            + '<b>X Range</b>     Min: <input type="text" ng-model="xmin" size="4">     Max: <input type="text" ng-model="xmax" size="4"> Interval: <input type="text" ng-model="xinterval" size="4"><br>'
+            + '<b>Y Range</b>     Min: <input type="text" ng-model="ymin" size="4">     Max: <input type="text" ng-model="ymax" size="4"> Interval: <input type="text" ng-model="yinterval" size="4"><br>'
+            + 'Set X and Y ranges automatically <input type="checkbox" ng-model="autoRange"><br>'
             + '<div id="container" style="width:600px;height:384px;margin:8px auto"></div>'
+            + '<input type="button" ng-click="getOutputDisplay()" value="Run">'
             + '</div>',
 controller: function($scope) {
 
@@ -34,7 +38,7 @@ $scope.colOptions = [];
 checkNumCol();
 
 function checkNumCol() {
-  var col, row, colOptions=[];
+  var col, row;
   for(col = 0; col < numCol; col++) {
     for(row = 0; row < numRecords; row++) {
       if(!isNumber(records[row][col])) {
@@ -42,12 +46,51 @@ function checkNumCol() {
       }
     }
     if(row==numRecords)
-      $scope.colOptions.push(colNames[col]);
+      $scope.colOptions.push({colIndex:col, colName:colNames[col]});
   }
 }
+
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+function checkRangeInput(range, min, max, interval) {
+  //console.log(range + " " +  min + " " + max + " " + interval );
+  if(!isNumber(min) || !isNumber(max) || !isNumber(interval) ) 
+    abortJS(range + " Error: Please enter numeric min, max and interval.");
+
+  min = parseFloat(min);
+  max = parseFloat(max);
+  interval = parseFloat(interval);
+  if(min > max) 
+    abortJS(range + " Error: max is smaller than min.");
+  
+  if(interval <= 0) 
+    abortJS(range + " Error: interval cannot be smaller or equals to zero.");
+  
+  var ticks = Math.ceil(Math.abs(max - min) / interval);
+  return [min, max, ticks];
+}
+
+function abortJS(err) {
+  alert(err);
+  throw new Error(err);
+}
+
+function getData(x, y) {
+  var
+    data = [],
+    row;
+
+  for(row = 0; row < numRecords; row++) {
+    data.push([records[row][x], records[row][y]]);
+  }
+  //console.log(data);
+  return data;
+}
+
+
+
 
 $scope.lineGroup=[];
 $scope.addLine = function(){
