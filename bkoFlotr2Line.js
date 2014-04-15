df3a7134dfe8c6be354867fdda00c1e3686ed0af
Di Wu <dw1157@nyu.edu>
2014-04-15 10:22:32 -0400

    LineGraph: change titles part
    Give default x, y and graph titles

--- bkoFlotr2Line.js
+++ bkoFlotr2Line.js
@@ -12,8 +12,11 @@
             +   '</ul>'
             +   '<div class="tab-content">'
             +     '<div class="tab-pane active" id="tab1">'
-            +       '<b>Title&nbsp;</b> <input type="text" ng-model="title"  size="30" placeholder="Add graph title here"></br>'
-            +       '<b>X Axis&nbsp;</b><select ng-model="xaxis" ng-options="colOption.colName for colOption in colOptions"><option value="">-- choose x-axis --</option></select></br>' 
+            +       '<table>'
+            +       '<tr><td><b>Title&nbsp;</b></td> <td><input type="text" ng-model="title"  size="30" placeholder="Add graph title here"></td></tr>'
+            +       '<tr><td><b>X Title&nbsp;</b></td> <td><input type="text" ng-model="xtitle"  size="30" placeholder="Add x-axis title here"></td><td><b>&nbsp;Y Title&nbsp;</b></td> <td><input type="text" ng-model="ytitle"  size="30" placeholder="Add y-axis title here"></td></tr>'
+            +       '<tr><td><b>X Axis&nbsp;</b></td><td><select ng-model="xaxis" ng-options="colOption.colName for colOption in colOptions"><option value="">-- choose x-axis --</option></select></td></tr>' 
+            +       '</table>'
             +       '<b>Y Axis&nbsp;</b>'
             +         '<table>' 
             +           '<tr ng-repeat="yOption in yAxisOptions"><td><input type="checkbox" ng-model="yOption.colSelected"></td><td>{{yOption.colName}}</td><td><input type="text" ng-model="yOption.colLabel" placeholder="Enter Line Label"></td></tr>'
@@ -36,7 +39,8 @@ var
     colNames = jsObj.columnNames,
     numCol = colNames.length,
     records = jsObj.values,
-    numRecords = records.length; //test which columns are numerical (numerical: true)
+    numRecords = records.length,
+    finalTitle, finalXTitle, finalYTitle; //test which columns are numerical (numerical: true)
 
 $scope.hideOrShow = " Hide ";
 $scope.displayConf = "display:block;";
@@ -75,14 +79,27 @@ function initializeYAxisOptions(){
 
 
 $scope.showGraph=function() {
+  //Get all selected y axis
   $scope.yaxis = [];
   for(var i in $scope.yAxisOptions) {
     if($scope.yAxisOptions[i].colSelected)
       $scope.yaxis.push($scope.yAxisOptions[i]);
   }
+
+  //Set title
+  if(needReset($scope.title)) finalTitle="Line Graph";
+  else finalTitle=$scope.title;
+  if(needReset($scope.xtitle)) finalXTitle=$scope.xaxis.colName;
+  else finalXTitle=$scope.xtitle;
+  if(needReset($scope.ytitle)) finalYTitle="Y";
+  else finalYTitle=$scope.ytitle;
   //if($scope.xaxis==undefined) abortJS("Error: please select x axis.");
   //if($scope.yaxis.length==0) abortJS("Error: please select at least one y axis.");
   getOutputDisplay();
+}
+
+function needReset(varStr) {
+  return (varStr==undefined||varStr==null||varStr=="");
 }
 
 function getData() {
@@ -90,7 +107,7 @@ function getData() {
 
   for (var i = 0; i < $scope.yaxis.length; i++) {
     var lb = $scope.yaxis[i].colLabel;
-    if(lb==undefined || lb=="") lb = $scope.yaxis[i].colName;
+    if(needReset(lb)) lb = $scope.yaxis[i].colName;
     data.push( {data:getOneLineData($scope.xaxis.colIndex, $scope.yaxis[i].colIndex), label: lb, lines:{show:true}, points:{show:true}});
   }
   console.log(data);
@@ -152,15 +169,15 @@ function getOutputDisplay(){
   }
 
   graph = Flotr.draw(container, data, {
-    title: $scope.title,
+    title: finalTitle,
     xaxis: {
-      title: xaxis.colName,
+      title: finalXTitle,
       min: xvals[0],
       max: xvals[1],
       noTicks: xvals[2]
     }, 
     yaxis: {
-      //title: colNames[colYIndex],
+      title: finalYTitle,
       min: yvals[0],
       max: yvals[1],
       noTicks: yvals[2]
