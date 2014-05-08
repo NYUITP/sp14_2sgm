f2e104542e743063fdb6b5f61a4ed3e2a0e99401
pallavimane <pallavi96@gmail.com>
2014-05-08 01:11:09 -0400

    Removed unused errors, Code Refactoring
    - Layout alignment with beaker cells issue fixed

--- bkoFlotr2Bubble.js
+++ bkoFlotr2Bubble.js
@@ -1,12 +1,12 @@
 //Author: Pallavi Mane
-//Date: 05/04/2014
+//Date: 05/08/2014
 (function () {
     'use strict';
     beaker.bkoDirective("flotr2Bubble",function () {
  return {
-	template:'<span id="inputerror" class="label label-important"></span><br>'
-	    + '<div id="container" style="width:600px;height:384px;margin-right:250px auto;float:left;display:inline-block"></div>'
-	    + '<div id="allcontent" style="width:600px;height:384px;display: inline-block;float:right;margin-left:5px"><div id="toDisplay">'
+	template:'<span id="inputerror" class="label label-important"></span><br><table cellpadding="4">'
+	    + '<tr><td valign="top"><div id="container" style="width:600px;height:384px"></div></td><td valign="top" id="configData">'
+	    + '<div id="allcontent" style="width:600px;height:384px"><div id="toDisplay">'
 	    + '<button id="button" class="btn btn-primary">Hide Configuration</button></div>'
 	    + '<div id="main">'
 	    + '<table cellpadding="2">'
@@ -19,11 +19,11 @@
 	    + '<tr><td><b>Bubble Size</b></td>'
             + '<td><select id=selectZ>'
             + '</select></td></tr>'
-            + '<tr><td><b>Title</b></td><td><input type="text" id="title" size="15"><br></td><tr>'
+            + '<tr><td><b>Title</b></td><td><input type="text" id="title" size="15"><br></td></tr>'
             + '<tr><td><b>Automatic Bounds</b></td><td><input type="checkbox" id="autoRange" checked="checked"><br></td></tr></table><br>'
 	    + '<div id="subcontent"><table cellpadding="2">'
             + '<tr><td><b>X Bound</b></td></tr> <tr><td>Min:</td><td> <input type="text" id="xmin" style="width:50px"><span id="errorxmin" class="label label-important"></span></td></tr>   <tr><td>Max:</td><td> <input type="text" id="xmax" style="width:50px"><span id="errorxmax" class="label label-important"></span></td></tr> <tr><td>Interval:</td><td> <input type="text" id="xinterval" style="width:50px"><span id="errorxinterval" class="label label-important"></span><br></td></tr>'
-            + '<tr><td><b>Y Bound</b> </td></tr><tr><td>Min:</td><td> <input type="text" id="ymin" style="width:50px"><span id="errorymin" class="label label-important"></span></td></tr>  <tr><td>Max:</td><td> <input type="text" id="ymax" style="width:50px"><span id="errorymax" class="label label-important"></span</td></tr> <tr><td>Interval:</td><td> <input type="text" id="yinterval" style="width:50px"><span id="erroryinterval" class="label label-important"></span><br></td></tr></table></div></div></div>',
+            + '<tr><td><b>Y Bound</b> </td></tr><tr><td>Min:</td><td> <input type="text" id="ymin" style="width:50px"><span id="errorymin" class="label label-important"></span></td></tr>  <tr><td>Max:</td><td> <input type="text" id="ymax" style="width:50px"><span id="errorymax" class="label label-important"></span</td></tr> <tr><td>Interval:</td><td> <input type="text" id="yinterval" style="width:50px"><span id="erroryinterval" class="label label-important"></span><br></td></tr></table></div></div></div></td></tr></table></table>',
 	link: function (scope) {
 
 			$("#toDisplay #button").click(function(){
@@ -45,6 +45,7 @@
 		    	numRecords = records.length,
 		    	isNumCol = checkNumCol();
 			var flag = 0;
+
 
 
 			/* Data Validation: check for columns with all numeric values */
@@ -81,13 +82,14 @@
 			if(totalNumCols < 3){
 				$('#inputerror').show();
 				$('#allcontent').hide();
+				$('#container').hide();
 				$('#inputerror').html("Bubble Chart Requires Minimum 3 numerical columns!");
 
 			}
 			else{
 	    			$('#inputerror').hide();
+				$('#container').show();
 				$('#allcontent').show();
-				drawChart();
 			}
 
 
@@ -96,12 +98,6 @@
 		  return !isNaN(parseFloat(n)) && isFinite(n);
 		}
 
-/******************** End Of Initialization and Data Validation *********************************/
-
-
-/********************** Dynamic Display of Bubble Chart **************************************/
-
-	function drawChart(){
 
 		fillDropdown("selectX");
 		fillDropdown("selectY");
@@ -129,6 +125,11 @@
 		$('#selectY').find('option:eq(2)').prop("selected", "selected");
 		$('#selectZ').find('option:eq(3)').prop("selected", "selected");
 		getOutputDisplay();
+
+/******************** End Of Initialization and Data Validation *********************************/
+
+
+/********************** Dynamic Display of Bubble Chart **************************************/
 
 
 		function dataToChart(x, y, z) {
@@ -161,10 +162,6 @@
 		$('#selectX').change(function(){
 			document.getElementById('selectX').selected=true;
 		if(($("#selectY :selected").text() != "") && ($("#selectZ :selected").text() != "")){
-			if($('#inputerror').is(':visible')){
-				$('#autoRange').prop("checked",true);
-				$('#inputerror').hide();
-			}
 			if($('#autoRange').is(':checked')) {
 					textDisable();
 			}
@@ -176,10 +173,6 @@
          	$('#selectY').change(function(){
 			document.getElementById('selectY').selected=true;
 		if(($("#selectX :selected").text() != "") && ($("#selectZ :selected").text() != "")){
-			if($('#inputerror').is(":visible")){
-				$('#autoRange').prop("checked",true);
-				$('#inputerror').hide();
-			}
 			if($('#autoRange').is(':checked')) {
 					textDisable();
 			}
@@ -190,10 +183,6 @@
 		$('#selectZ').change(function(){
 			document.getElementById('selectZ').selected=true;
 		if(($("#selectX :selected").text() != "") && ($("#selectY :selected").text() != "")){
-			if($('#inputerror').is(':visible')){
-				$('#autoRange').prop("checked",true);
-				$('#inputerror').hide();
-			}
 			if($('#autoRange').is(':checked')) {
 					textDisable();
 			}
@@ -210,15 +199,16 @@
 		if($('#autoRange').is(':checked')){
 			$('#subcontent').hide();
 			if(($("#selectX :selected").text() != "") && ($("#selectZ :selected").text() != "") && ($("#selectZ :selected").text() != "")){
+				$('#configData').css("height","384px");
 				errortextHide();
 				textDisable();
 				getOutputDisplay();
 			}
 		    }
 		else{
+			$('#configData').css("height","700px");
 			$('#subcontent').show();
 			if(($("#selectX :selected").text() != "") && ($("#selectZ :selected").text() != "") && ($("#selectZ :selected").text() != "")){
-				$('#inputerror').hide();
 				textEnable();
 				getOutputDisplay();
 			}
@@ -413,8 +403,6 @@
 			}
 		
 		    }
-
-         }
 /********************** End Of Dynamic Display of Bubble Chart **************************************/
 	}
 
